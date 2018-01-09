0c83a087c3aed6205f51fb269a3258b183304b85
Jo Dickson <jogreybill@gmail.com>
2018-01-09 11:30:18 -0600

    Removing default container-wrapped singular template due to incompatibilities with container-wrapped sections and section menus

--- singular.php
+++ singular.php
@@ -1,9 +1,7 @@
 <?php get_header(); the_post(); ?>
 
 <article class="<?php echo $post->post_status; ?> post-list-item mb-5">
-	<div class="container">
-		<?php the_content(); ?>
-	</div>
+	<?php the_content(); ?>
 </article>
 
 <?php get_footer(); ?>
