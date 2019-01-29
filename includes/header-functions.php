48548ab42f45e03dbd465242aea0deb0f3f08ab2
Jo Dickson <jogreybill@gmail.com>
2019-01-29 15:33:47 -0500

    Fixed issue in ucfwp_get_header_title() where author and date-based archives wouldn't use the correct title text

--- includes/header-functions.php
+++ includes/header-functions.php
@@ -104,12 +104,10 @@ function ucfwp_get_header_videos( $obj ) {
 		return wptexturize( $title );
 	}
 
-	if ( ! $obj ) {
+	if ( is_404() ) {
 		// We intentionally don't add a fallback title for 404s;
 		// this allows us to add a custom h1 to the default 404 template.
-		if ( ! is_404() ) {
-			$title = get_bloginfo( 'name', 'display' );
-		}
+		$title = '';
 	}
 	else {
 		// Checks listed below are copied directly from WP core
