e10b1d72b0746052266abf0801a098a10d984e44
Cadie Brown <cadie.brown@gmail.com>
2019-03-19 18:10:39 -0400

    Do not display header element if custom content is empty

--- header.php
+++ header.php
@@ -8,10 +8,12 @@
 		<div id="ucfhb"></div>
 
 		<?php do_action( 'after_body_open' ); ?>
-
-		<header class="site-header">
+		
+		<?php if ( ucfwp_check_header_custom_content() ) : ?>
+		<header class="site-header test">
 			<?php echo ucfwp_get_header_markup(); ?>
 		</header>
+		<?php endif; ?>
 
 		<main class="site-main">
 			<?php echo ucfwp_get_subnav_markup(); ?>
